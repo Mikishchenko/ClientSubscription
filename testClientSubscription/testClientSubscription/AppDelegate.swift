64cfd69b9de303627132bda71057be6dc9ad4420
Mikishchenko <vovamik@mail.ru>
2019-08-20 23:30:08 +0300

    start without Launchscreen and VC in Mainstoryboard

--- testClientSubscription/testClientSubscription/AppDelegate.swift
+++ testClientSubscription/testClientSubscription/AppDelegate.swift
@@ -14,6 +14,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        let navController = UINavigationController(rootViewController: CSController())
+        navController.isNavigationBarHidden = true
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = navController
+        window?.makeKeyAndVisible()
+        
         return true
     }
 }
